793ec73bfe8e14f27e3dd6b07b073503d9847c3a
young5552 <54834694+young5552@users.noreply.github.com>
2020-06-05 17:17:45 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,28 @@ function setUserName() {
       myHeading.textContent = '爱你，' + myName;
   }
 }
+/*判断上下滑动：*/
+$('body').bind('touchstart', function (e) {
+	startX = e.originalEvent.changedTouches[0].pageX;
+	startY = e.originalEvent.changedTouches[0].pageY;
+});
+$("body").bind("touchmove", function (e) {
+	//获取滑动屏幕时的X,Y
+	endX = e.originalEvent.changedTouches[0].pageX;
+	endY = e.originalEvent.changedTouches[0].pageY;
+	//获取滑动距离
+	distanceX = endX - startX;
+	distanceY = endY - startY;
+	//判断滑动方向
+	if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX > 0) {
+		console.log('往右滑动');
+	} else if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX < 0) {
+		console.log('往左滑动');
+	} else
+		if (Math.abs(distanceX) < Math.abs(distanceY) && distanceY < 0) {
+			console.log('往上滑动');
+		} else if (Math.abs(distanceX) < Math.abs(distanceY) && distanceY > 0) {
+			console.log('往下滑动');
+
+		}
+})
